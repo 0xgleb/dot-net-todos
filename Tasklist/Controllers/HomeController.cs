25c8f47d99eab3d40f8792ac1dc37ba2e64a31a7
David Dianov <uti55531@gmail.com>
2016-07-02 20:56:06 +0100

    initial commit

---
+++ Tasklist/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tasklist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<string> Tasks = new List<string>();
+
+        public ActionResult Index()
+        {
+            ViewBag.Tasks = Tasks;
+            return View();
+        }
+
+        public ActionResult AddTask()
+        {
+             var form = Request.Form;
+
+            if (!string.IsNullOrEmpty(form["task"]))
+                Tasks.Add(@form["task"]);
+
+            return Redirect("Index");
+        }
+    }
+}
