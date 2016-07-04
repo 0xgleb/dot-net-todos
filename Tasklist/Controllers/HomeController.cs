87cd0ecf9f63cd97f9eca846de52b29aed856619
david <uti55531@gmail.com>
2016-07-04 14:16:59 +0100

    Almost done with db

--- Tasklist/Controllers/HomeController.cs
+++ Tasklist/Controllers/HomeController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tasklist.Models;
 
 namespace Tasklist.Controllers
 {
     public class HomeController : Controller
     {
         // TODO: connect a db
-        public static Tasks db = new Tasks(); // TODO: create a model
+        public static TasksEntities db = new TasksEntities(); // TODO: create a model
 
         // public static List<string> Tasks = new List<string>();
 
         public ActionResult Index()
         {
-            var Tasks = db.Where(x => x.Value != "" || x.Value != null || x.Value.Split(' ') != null).ToList(); // check last condition in Where
-            ViewBag.Tasks = Tasks;
+            var Tasklist = from x in db.TaskTables
+                           where x.Task != ""
+                           select x.Task;
+
+            ViewBag.Tasks = Tasklist;
             return View();
         }
 
@@ -24,8 +28,8 @@ namespace Tasklist.Controllers
         {
             var form = Request.Form;
 
-            if (!string.IsNullOrEmpty(form["task"]))
-                Tasks.Add(@form["task"]);
+            //if (!string.IsNullOrEmpty(form["task"]))
+                //db.(@form["task"]);
 
             return Redirect("Index");
         }
